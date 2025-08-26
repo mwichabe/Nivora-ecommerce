a63895b149f6a66237ef5e9520bcb2ba84249bea
mwichabe <mwichabecollins@gmail.com>
2025-08-26 15:35:58 +0300

    Nivora client

---
+++ client/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Common/Header'
+
+const UserLayout = () => {
+  return <>
+    <Header/>
+    {/*Main Content */}
+    {/*Footer */}
+  </>
+}
+export default UserLayout;
