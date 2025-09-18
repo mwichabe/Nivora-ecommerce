6a47bddaef4b3a4493f523921de2dec14a2bf1c3
mwichabe <mwichabecollins@gmail.com>
2025-09-18 21:36:45 +0300

    All

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Layout/Hero';
 import GenderCollectionSection from '../components/Products/GenderCollectionSection';
 import NewArrivals from '../components/Products/NewArrivals';
+import ProductDetail from '../components/Products/ProductDetail';
 
  const Home = () => {
   return (
@@ -9,6 +10,11 @@ import NewArrivals from '../components/Products/NewArrivals';
         <Hero />
         <GenderCollectionSection />
         <NewArrivals />
+        {/* Best seller */}
+        <h2 className="text-3xl text-center font-bold mb-4">
+          Best Seller
+          <ProductDetail />
+        </h2>
     </div>
   )
 }
